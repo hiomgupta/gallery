1553e5b231d48bfb39ddbef8ed9cad4791024ca7
Om Gupta <136147194+hiomgupta@users.noreply.github.com>
2026-07-12 02:56:17 +0530

    push images to web by using node generate-gallery.js

--- generate-gallery.js
+++ generate-gallery.js
@@ -3,25 +3,42 @@ const path = require('path');
 
 // Point this to your images directory
 const imageDir = path.join(__dirname, 'images');
+const jsonFile = path.join(__dirname, 'projects.json');
 
 // Read the folder
 const files = fs.readdirSync(imageDir);
 
-const projects = files
-  .filter(file => file.match(/\.(jpg|jpeg|png|webp|gif)$/i)) // Only keep images
-  .map(file => {
-    // Remove the file extension and replace dashes/underscores with spaces
-    let title = file.replace(/\.[^/.]+$/, "").replace(/[-_]/g, " ");
-    
-    // Capitalize the first letter of each word
-    title = title.replace(/\b\w/g, char => char.toUpperCase());
+function projectAlreadyExists(projects, imagePath) {
+  return projects.some(project => project.image.toLowerCase() === imagePath.toLowerCase());
+}
 
-    return {
-      title: title,
-      image: `images/${file}` // The path your HTML needs to find the image
-    };
+function formatTitleFromFilename(file) {
+  let title = file.replace(/\.[^/.]+$/, "").replace(/[-_]/g, " ");
+  return title.replace(/\b\w/g, char => char.toUpperCase());
+}
+
+let projects = [];
+if (fs.existsSync(jsonFile)) {
+  try {
+    projects = JSON.parse(fs.readFileSync(jsonFile, 'utf8')) || [];
+  } catch (error) {
+    console.error(`Error reading existing ${path.basename(jsonFile)}. Overwriting with a fresh list.`);
+    projects = [];
+  }
+}
+
+files
+  .filter(file => file.match(/\.(jpg|jpeg|png|webp|gif)$/i))
+  .forEach(file => {
+    const image = `images/${file}`;
+    if (!projectAlreadyExists(projects, image)) {
+      projects.push({
+        title: formatTitleFromFilename(file),
+        image: image
+      });
+    }
   });
 
 // Save it to a JSON file in your main folder
-fs.writeFileSync('projects.json', JSON.stringify(projects, null, 2));
-console.log(`Success! Created projects.json with ${projects.length} images.`);
+fs.writeFileSync(jsonFile, JSON.stringify(projects, null, 2));
+console.log(`Success! Updated ${path.basename(jsonFile)} with ${projects.length} images.`);
